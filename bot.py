492e6106776bc593b77783b2dfee4ce985bbaf81
K Hari Aneesh Siddhartha <78897025+ansid0102@users.noreply.github.com>
2021-12-17 23:24:50 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,91 @@
+
+#Meet bot: your friend
+
+#import necessary libraries
+import io
+import random
+import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading packages
+
+nltk.download('punkt') # first-time use only
+nltk.download('wordnet') # first-time use only
+
+
+Reading in the corpus
+with open('VIT.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+#TOkenisation
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# Generating response
+def response(user_response):
+    bot_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        bot_response=bot_response+"I am sorry! I don't understand you"
+        return bot_response
+    else:
+        bot_response = bot_response+sent_tokens[idx]
+        return bot_response
+
+
+flag=True
+print("VITBOT: HELLO I'm VIT BOT! Ask me Questions !Type Bye to exit ")
+while(flag==True):
+    user_response = input("Question: ")
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("VITBOT: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("VITBOT: "+greeting(user_response))
+            else:
+                print("VITBOT: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("VITBOT: Bye! take care..")    
+        
+        
+
